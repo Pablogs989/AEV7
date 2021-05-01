ff5fcf3bc009082913efa24e7deffc05831c7f4a
jjjoseee <jomale@floridauniversitaria.es>
2021-05-01 16:25:47 +0200

    coza

--- AEV7/AEV7/Form1.Designer.cs
+++ AEV7/AEV7/Form1.Designer.cs
@@ -30,22 +30,14 @@ namespace AEV7
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.txtRelojH = new System.Windows.Forms.Label();
             this.txtRelojF = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::AEV7.Properties.Resources.logo;
-            this.pictureBox1.Location = new System.Drawing.Point(68, 116);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(275, 400);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // timer1
             // 
@@ -55,34 +47,59 @@ namespace AEV7
             // txtRelojH
             // 
             this.txtRelojH.AutoSize = true;
-            this.txtRelojH.Font = new System.Drawing.Font("Bahnschrift Light", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtRelojH.Location = new System.Drawing.Point(163, 64);
+            this.txtRelojH.Font = new System.Drawing.Font("Source Sans Pro Light", 16.25F);
+            this.txtRelojH.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtRelojH.Location = new System.Drawing.Point(164, 89);
             this.txtRelojH.Name = "txtRelojH";
-            this.txtRelojH.Size = new System.Drawing.Size(63, 28);
+            this.txtRelojH.Size = new System.Drawing.Size(55, 28);
             this.txtRelojH.TabIndex = 1;
             this.txtRelojH.Text = "Hora";
             // 
             // txtRelojF
             // 
             this.txtRelojF.AutoSize = true;
-            this.txtRelojF.Font = new System.Drawing.Font("Bahnschrift Light", 17.25F);
-            this.txtRelojF.Location = new System.Drawing.Point(153, 21);
+            this.txtRelojF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.txtRelojF.Font = new System.Drawing.Font("Source Sans Pro Light", 16.25F);
+            this.txtRelojF.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtRelojF.Location = new System.Drawing.Point(155, 52);
             this.txtRelojF.Name = "txtRelojF";
-            this.txtRelojF.Size = new System.Drawing.Size(73, 28);
+            this.txtRelojF.Size = new System.Drawing.Size(66, 28);
             this.txtRelojF.TabIndex = 2;
             this.txtRelojF.Text = "Fecha";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::AEV7.Properties.Resources.lineaHF;
+            this.pictureBox2.Location = new System.Drawing.Point(68, 65);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(275, 48);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AEV7.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(68, 180);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(928, 668);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.ClientSize = new System.Drawing.Size(934, 761);
             this.Controls.Add(this.txtRelojF);
             this.Controls.Add(this.txtRelojH);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -95,6 +112,7 @@ namespace AEV7
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label txtRelojH;
         private System.Windows.Forms.Label txtRelojF;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- AEV7/AEV7/Properties/Resources.Designer.cs
+++ AEV7/AEV7/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace AEV7.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap lineaHF {
+            get {
+                object obj = ResourceManager.GetObject("lineaHF", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap logo {
             get {
                 object obj = ResourceManager.GetObject("logo", resourceCulture);
